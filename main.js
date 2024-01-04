8024d658a104f0a5f35bdc7d0628685ad9451651
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-04 23:41:24 +0100

    Integrated Player Class in Main
    In this update, the Player class is imported into the main.js file. The Game class has been expanded to include a new player instance. Additionally, the game instance is created after defining the Game class.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { Player } from './player.js';
+
 window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
@@ -8,6 +10,14 @@ window.addEventListener('load', function () {
         constructor(width, height) {
             this.width = width;
             this.height = height;
+            this.player = new Player(this);
+        }
+        update() {}
+
+        draw(context) {
+            this.player.draw(context);
         }
     }
+
+    const game = new Game(canvas.width, canvas.height);
 });
