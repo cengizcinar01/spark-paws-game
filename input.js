34a3a7ec22adece92180a72b17c0842ce0ea8395
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-05 23:07:52 +0100

    Improved Key Event Handling
    The existing key event handling was updated in the InputHandler class. The 'keydown' event listener now also listens for 'ArrowUp' key presses. Additionally, a new 'keyup' event listener was added to remove the key from the keys array when the 'ArrowDown' or 'ArrowUp' keys are released.

--- input.js
+++ input.js
@@ -2,9 +2,16 @@ export class InputHandler {
     constructor() {
         this.keys = [];
         window.addEventListener('keydown', (e) => {
-            console.log(e.key, this.keys);
-            if (e.key === 'ArrowDown' && this.keys.indexOf(e.key) === -1) {
+            if ((   e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' 
+                ) && this.keys.indexOf(e.key) === -1) {
                 this.keys.push(e.key);
+            }
+            console.log(e.key, this.keys);
+        });
+        window.addEventListener('keyup', (e) => {
+            if (e.key === 'ArrowDown' || e.key === "ArrowUp") {
+                this.keys.splice(this.keys.indexOf(e.key), 1);
             }
         });
     }
