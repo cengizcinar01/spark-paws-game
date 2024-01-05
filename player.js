92e0ee5cfc5578d493347bb97f03888e3d6e10b3
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-05 23:30:10 +0100

    Player Vertical Movement Improvement
    The player's vertical movement in the game has been refined. The changes include adjusting the velocity and ensuring the player stays grounded when not moving.

--- player.js
+++ player.js
@@ -20,9 +20,10 @@ export class Player {
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
+        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 20;
         this.y += this.vy;
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 10;
         if (!this.onGround()) this.vy += this.weight;
+        else this.vy = 0;
     }
     draw(context) {
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
