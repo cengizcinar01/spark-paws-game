2021c83e8c9505e1e5537352ce23e00558ab752a
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-07 23:54:57 +0100

    Increase enemy speed with game speed

--- enemies.js
+++ enemies.js
@@ -9,7 +9,7 @@ class Enemy {
     }
     update(deltaTime) {
         // movement
-        this.x -= this.speedX;
+        this.x -= this.speedX + this.game.speed;
         this.y += this.speedY;
         if (this.frameTimer > this.frameInterval) {
             this.frameTimer = 0;
