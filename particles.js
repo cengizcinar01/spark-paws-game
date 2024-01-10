c48682d6474b9725f406d637eaf04753a592d73b
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-10 23:30:44 +0100

    Add Dust particle constructor and draw
    This allows for the creation of Dust particles with randomized size, speed color, and their subsequent rendering.

--- particles.js
+++ particles.js
@@ -12,14 +12,23 @@ class Particle {
 }
 
 export class Dust extends Particle {
-    
+    constructor(game, x, y) {
+        super(game);
+        this.size = Math.random() * 10 + 10;
+        this.x = x;
+        this.y = y;
+        this.speedX = Math.random();
+        this.speedY = Math.random();
+        this.color = 'black';
+    }
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        context.fillStyle = this.color;
+        context.fill();
+    }
 }
 
-export class Splash extends Particle {
-    
-}
+export class Splash extends Particle {}
 
-export class Fire extends Particle {
-    
-}
-
+export class Fire extends Particle {}
