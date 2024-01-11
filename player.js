f09fb34441cbabf5a8016f6bb7a550f3da725fe8
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-11 22:32:01 +0100

    Add Diving state to Player class
    This was added to the imported states and to the states array in the Player constructor.

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling, Rolling } from './playerStates.js';
+import { Sitting, Running, Jumping, Falling, Rolling, Diving } from './playerStates.js';
 
 export class Player {
     constructor(game) {
@@ -18,7 +18,7 @@ export class Player {
         this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game), new Falling(this.game), new Rolling(this.game)];
+        this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game), new Falling(this.game), new Rolling(this.game), new Diving(this.game)];
     }
     update(input, deltaTime) {
         this.checkCollision();
