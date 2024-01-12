6a603d2a1e836507bf35db67b7efff81ab172f79
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-12 23:44:12 +0100

    Add Hit state to Player class
    This state is triggered when the player collides with an enemy, unless the player is rolling or diving.

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling, Rolling, Diving } from './playerStates.js';
+import { Sitting, Running, Jumping, Falling, Rolling, Diving, Hit } from './playerStates.js';
 
 export class Player {
     constructor(game) {
@@ -18,7 +18,15 @@ export class Player {
         this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game), new Falling(this.game), new Rolling(this.game), new Diving(this.game)];
+        this.states = [
+            new Sitting(this.game),
+            new Running(this.game),
+            new Jumping(this.game),
+            new Falling(this.game),
+            new Rolling(this.game),
+            new Diving(this.game),
+            new Hit(this.game),
+        ];
     }
     update(input, deltaTime) {
         this.checkCollision();
@@ -62,9 +70,11 @@ export class Player {
         this.game.enemies.forEach((enemy) => {
             if (enemy.x < this.x + this.width && enemy.x + enemy.width > this.x && enemy.y < this.y + this.height && enemy.y + enemy.height > this.y) {
                 enemy.markedForDeletion = true;
-                this.game.score++;
-            } else {
-                //no collision
+                if (this.currentState === this.states[4] || this.currentState === this.states[5]) {
+                    this.game.score++;
+                } else {
+                    this.setState(6, 0);
+                }
             }
         });
     }
