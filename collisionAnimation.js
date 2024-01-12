d80acf3aca6a4bac8993492ce1e1775c4c5619f3
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-12 23:55:13 +0100

    Add sprite dimensions to collisionAnimation
    Added spriteWidth and spriteHeight to the collisionAnimation class. Also included a sizeModifier to randomly adjust the sprite's size for variety in the animation.

--- collisionAnimation.js
+++ collisionAnimation.js
@@ -2,5 +2,10 @@ class collisionAnimation {
     constructor(game, x, y) {
         this.game = game;
         this.image = document.getElementById('collisionAnimation');
+        this.spriteWidth = 100;
+        this.spriteHeight = 90;
+        this.sizeModifier = Math.random() + 0.5;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
     }
 }
