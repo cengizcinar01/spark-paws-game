de861f5c96c9c9099780e33c35b8e0a315981d44
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-13 21:25:42 +0100

    Add game timer and messages
    The timer displays the game time, while the game over messages appear when the game ends.

--- UI.js
+++ UI.js
@@ -10,5 +10,14 @@ export class UI {
         context.fillStyle = this.game.fontColor;
         // score
         context.fillText('Score: ' + this.game.score, 20, 50);
+        // timer
+        context.font = this.fontSize * 0.8 + ' px' + this.fontFamily;
+        context.fillText('Time: ' + this.game.time, 20, 80);
+        // game over messages
+        if (this.game.gameOver) {
+            context.textAlign = 'center';
+            context.font = this.fontSize * 2 + ' px' + this.fontFamily;
+            context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5);
+        }
     }
 }
