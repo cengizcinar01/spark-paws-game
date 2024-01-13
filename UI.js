8c983c2c82ee5e782efbb6d6478b41ba19c212d9
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-13 21:34:25 +0100

    Update UI game over messages
    Updated the game over messages in the UI.js file to display different messages based on the player's score.

--- UI.js
+++ UI.js
@@ -11,13 +11,21 @@ export class UI {
         // score
         context.fillText('Score: ' + this.game.score, 20, 50);
         // timer
-        context.font = this.fontSize * 0.8 + ' px' + this.fontFamily;
+        context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
         context.fillText('Time: ' + this.game.time, 20, 80);
         // game over messages
         if (this.game.gameOver) {
             context.textAlign = 'center';
-            context.font = this.fontSize * 2 + ' px' + this.fontFamily;
-            context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5);
+            context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+            if (this.game.score > 5) {
+                context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('What do night creatures fear? YOU!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            } else {
+                context.fillText('Love at first bite?', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('Nope. Better luck next time!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            }
         }
     }
 }
